205c38207cc6d9eb221685d0a901cb2d2f9fc113
hsu win latt <lareina1467@gmail.com>
2025-08-27 12:33:38 +0700

    feat: gsap dra

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Draggable } from "gsap/all";
+
+gsap.registerPlugin(Draggable);
+
 const App = () => {
-  return <div>App</div>;
+  useGSAP(() => {
+    Draggable.create("#card", {
+      bounds: ".bounding",
+    });
+  });
+
+  return (
+    <section className="flex justify-center items-center h-dvh overflow-hidden bg-black bounding">
+      <h1
+        id="card"
+        className="text-3xl font-bold underline bg-blue-100 text-blue-600 p-10 rounded-md w-fit hover:bg-green-600 hover:text-green-100"
+      >
+        Hello world!
+      </h1>
+    </section>
+  );
 };
 
 export default App;
